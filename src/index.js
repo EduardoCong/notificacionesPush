d4e9c70da31ef47e848be67acfc26aac86e11489
Eduardo Cong <alejandro.cong13@gmail.com>
2024-07-22 00:20:46 -0600

    Fix 5

--- src/index.js
+++ src/index.js
@@ -10,55 +10,50 @@ const app = express();
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: "*",
-  })
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    })
 );
 
-app.use(
-  cors({
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
-
-app.use((res, next)=> {
-  res.setHeader("Content-Type", "application/json");
-  next();
+app.use((req, res, next) => {
+    res.setHeader("Content-Type", "application/json");
+    next();
 });
 
 initializeApp({
     credential: applicationDefault(),
     projectId: "pushnotis-50040",
-  });
-  
-
-app.post("/send", (req, res) => {
-  const receivedToken = req.body.fcmToken;
-  const message = {
-    notification: {
-      title: "¡Es tu su turno!",
-      body: "Pase al ande 5",
-    },
-    token:
-      "ca8t2w4dTeChqPwFE9rioy:APA91bEPnHeC4aLK3cSGGPfLqmQ7g5D3HFzmdHxNe44e725b44e_gJ-qid1Ro7EMdTg4L8YL7442o7UZHMO1tVCrfjHzUsRug21h2RnpkfIdBr93YaKTw18EneOywPN14rvz3WvNcICg",
-  };
-
-  getMessaging()
-    .send(message)
-    .then((response) => {
-      res.status(200).json({
-        message: "Mensaje enviado con exito",
-        token: receivedToken,
-      });
-      console.log("Mensaje enviado", response);
-    })
-    .catch((error) => {
-      res.status(400);
-      res.send(error);
-      console.log("Error al enviar mensaje", error);
-    });
 });
 
-app.listen(PORT, ()=>{
+
+app.post("/send", (req, res) => {
+    const receivedToken = req.body.fcmToken;
+    const message = {
+        notification: {
+            title: "¡Es tu su turno!",
+            body: "Pase al ande 5",
+        },
+        token:
+            "ca8t2w4dTeChqPwFE9rioy:APA91bEPnHeC4aLK3cSGGPfLqmQ7g5D3HFzmdHxNe44e725b44e_gJ-qid1Ro7EMdTg4L8YL7442o7UZHMO1tVCrfjHzUsRug21h2RnpkfIdBr93YaKTw18EneOywPN14rvz3WvNcICg",
+    };
+
+    getMessaging()
+        .send(message)
+        .then((response) => {
+            res.status(200).json({
+                message: "Mensaje enviado con exito",
+                token: receivedToken,
+            });
+            console.log("Mensaje enviado", response);
+        })
+        .catch((error) => {
+            res.status(400);
+            res.send(error);
+            console.log("Error al enviar mensaje", error);
+        });
+});
+
+app.listen(PORT, () => {
     console.log("server on port", PORT);
 });
