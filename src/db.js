3bbeb43d40b28d1a645e4411b28e83e376d97002
Eduardo Cong <alejandro.cong13@gmail.com>
2024-07-23 19:31:15 -0600

    AHora si

--- src/db.js
+++ src/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: process.env.host,
-  user: process.env.user,
-  password: process.env.contrasena,
-  database: process.env.basededatos,
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_control_turnos",
 });
 
 export default pool;
